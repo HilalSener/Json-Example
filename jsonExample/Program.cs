046814d092c08408f5f0ebd98a5a0b88664eaf4c
HilalSener <crescent.forest@hotmail.com>
2016-06-06 02:16:20 +0300

    First json example

---
+++ jsonExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("*** Open Weather json Data ***");
+
+            using (WebClient wc = new WebClient())
+            {
+                var uri = ("http://api.openweathermap.org/data/2.5/forecast/city?id=745044&APPID={APIKEY}");
+                var json = wc.DownloadString(uri);
+
+                var djson = JsonConvert.DeserializeObject(json);
+                Console.WriteLine(djson);
+            }
+        }
+    }
+}
