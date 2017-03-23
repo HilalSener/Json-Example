68181af60dc0546e45eeedd61ea6b53e35fe542f
HilalSener <crescent.forest@hotmail.com>
2017-03-23 21:23:19 +0300

    JsonDemoWithHttpClient added.

---
+++ JsonDemoWithHttpClient/Model/EarthquakeModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDemoWithHttpClient.Model
+{
+    public class Earthquake
+    {
+        public string Date { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Deepth { get; set; }
+        public double MD { get; set; }
+        public double ML { get; set; }
+        public double MW { get; set; }
+        public string Location { get; set; }
+        public string Result { get; set; }
+    }
+
+    public class EarthquakeModel
+    {
+        public List<Earthquake> Earthquakes { get; set; }
+        //public List<object> Errors { get; set; }
+    }
+}
---
+++ JsonDemoWithHttpClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using JsonDemoWithHttpClient.Model;
+using System.Collections.ObjectModel;
+
+namespace JsonDemoWithHttpClient
+{
+    public class Program
+    {
+        private static string Uri = "http://sondepremler20160728065405.azurewebsites.net/last";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***** Datalar Getiriliyor *****");
+
+            Task.Run(async () =>
+            {
+                await GetEarthquke();
+
+            }).GetAwaiter().GetResult();
+        }
+
+        public static async Task<EarthquakeModel> GetEarthquke()
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, Uri);
+                client.DefaultRequestHeaders.Add("Accept", "appliction/json");
+                var response = await client.SendAsync(request);
+                var result = response.Content.ReadAsStringAsync().Result;
+                var myreturn = JsonConvert.DeserializeObject<EarthquakeModel>(result);
+
+                Console.WriteLine(myreturn);
+                Console.ReadKey();
+
+                return myreturn;
+            }
+        }
+    }
+}
